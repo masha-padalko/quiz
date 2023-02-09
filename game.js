1d6712c5d9850c532a66fc8ca7c29a1d5226e299
padalko.m <padalko.m@soft2bet.com>
2023-02-09 11:52:25 +0200

    quiz complted

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 let currentQuestion = {};
 let accceptingQuestions = true;
@@ -7,33 +12,42 @@ let score = 0;
 let questionCounter = 0;
 let avaliableQuestions = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1,
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3,
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4,
-  },
-];
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10")
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions.results);
+
+    questions = loadedQuestions.results.map((loadedQuestions) => {
+      const formattedQuestion = {
+        question: loadedQuestions.question,
+      };
+
+      const answerChoices = [...loadedQuestions.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestions.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
+    startGame();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // constants
 
@@ -44,16 +58,24 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   avaliableQuestions = [...questions];
-  console.log(avaliableQuestions);
   getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
   if (avaliableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
     // go to the end page
     return window.location.assign("/end.html");
   }
   questionCounter++;
+
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  // update progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
   const questionIndex = Math.floor(Math.random() * avaliableQuestions.length);
   currentQuestion = avaliableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -73,8 +95,21 @@ choices.forEach((choice) => {
     accceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
-    getNewQuestion();
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
-startGame();
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
